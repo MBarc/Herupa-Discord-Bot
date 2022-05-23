5ca81327aae601d77bff36f4dd840d8b624ef681
MBarc <42979055+MBarc@users.noreply.github.com>
2022-05-23 00:49:28 -0400

    Add files via upload

---
+++ cogs/temp.py
@@ -0,0 +1,46 @@
+#
+
+import discord
+from discord.ext import commands
+
+class Temp(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def temp(self, ctx):
+
+        print("temp triggered")
+
+
+        #target = await ctx.message.channel.fetch_message(727219710428708947)
+
+        #await target.add_reaction('0️⃣')
+        #await target.add_reaction('1️⃣')
+        #await target.add_reaction('2️⃣')
+        #await target.add_reaction('3️⃣')
+        #await target.add_reaction('4️⃣')
+        #await target.add_reaction('5️⃣')
+        #await target.add_reaction('6️⃣')
+        #await target.add_reaction('7️⃣')
+        #await target.add_reaction('8️⃣')
+        #await target.add_reaction('9️⃣')
+        #await target.add_reaction('🔟')
+
+        #await target.add_reaction('❤') # red heart
+        #await target.add_reaction('💙') # blue heart
+        #await target.add_reaction('💚') # green heart
+        #await target.add_reaction('💜') # purple heart
+        #await target.add_reaction('💛') # yellow heart
+
+        #await target.add_reaction('👨‍🦳') # yellow heart
+
+        print("before")
+        await ctx.message.channel.send("old")
+        print("after")
+
+
+
+def setup(client):
+    client.add_cog(Temp(client))
