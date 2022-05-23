1b6f5d300a4a8d9dde4bdce3e81b5ba42fd655c7
MBarc <42979055+MBarc@users.noreply.github.com>
2022-05-23 02:22:56 -0400

    Update bitch.py

--- cogs/bitch.py
+++ cogs/bitch.py
@@ -29,7 +29,7 @@ class Bitch(commands.Cog):
         herupaErrorLogChannel = self.client.get_channel(configFile()["herupaErrorLogChannel"])
         await herupaErrorLogChannel.send(f"{commandName.upper()} error: {error}")
         
-# super testing
+# testing super late at night
 
 def setup(client):
     client.add_cog(Bitch(client))
