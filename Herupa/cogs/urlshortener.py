93b744d9b57d13def94e9ff2a54d23945a4a021f
MBarc <42979055+MBarc@users.noreply.github.com>
2023-05-29 15:31:03 -0400

    Update urlshortener.py

--- Herupa/cogs/urlshortener.py
+++ Herupa/cogs/urlshortener.py
@@ -55,5 +55,5 @@ class URLShortener(commands.Cog):
         herupaErrorLogChannel = self.client.get_channel(configFile()["herupaErrorLogChannel"])
         await herupaErrorLogChannel.send(f"{commandName.upper()} error: {error}")
 
-def setup(client):
-    client.add_cog(URLShortener(client))
+async def setup(client):
+    await client.add_cog(URLShortener(client))
